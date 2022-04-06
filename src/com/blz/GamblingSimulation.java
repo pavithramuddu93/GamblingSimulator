1cb9440d5f6525255e4afda07c9b1fb833bf6d35
pavithramuddu93 <pavithramuddu93>
2022-04-06 21:16:39 +0530

    UC2 added

--- src/com/blz/GamblingSimulation.java
+++ src/com/blz/GamblingSimulation.java
@@ -1,12 +1,29 @@
 package com.blz;
+import java.util.Random;
 
 public class GamblingSimulation {
-    static final int STAKE_PER_DAY = 100;
-    static final int BET_PER_GAME = 1;
+    public static final int STAKE_PER_DAY = 100;
+    public static final int BET_PER_GAME = 1;
+    public static final int WIN = 1;
+    public static int stake = 0;/// intial gambler bets with 1
+
+    public static void winCheck() {
+        Random random = new Random();
+        int betReturns = random.nextInt(2);
+        if (betReturns == WIN) {
+            stake++;// incrementing
+            System.out.println("win");
+        } else {
+            stake--;// decrementing
+            System.out.println("loss");
+        }
+        System.out.println("stake = " + stake);
+
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulation");
-
+        winCheck();// calling function
     }
 
 }
